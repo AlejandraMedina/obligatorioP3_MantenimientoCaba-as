75724d4db309c1b83a728142baed8d00423573c0
Alejandra Medina <naff2121@gmail.com>
2023-06-17 12:42:27 -0300

    prueba git

--- Dominio/InterfacesRepositorios/IRepositorioTipos.cs
+++ Dominio/InterfacesRepositorios/IRepositorioTipos.cs
@@ -10,6 +10,8 @@ namespace Dominio.InterfacesRepositorios
 {
     public interface IRepositorioTipos : IRepositorio<Tipo>
     {
+
+        //prueba para git borrar esta linea
         //Tipo BuscarTipoPorNombre(string nombre);
 
     }
