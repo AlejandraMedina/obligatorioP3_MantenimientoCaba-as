0227d29332aa199146dfd0ef0e2df374f07fb36b
jibentancor <joseignaciobentancor@gmail.com>
2023-06-18 20:44:34 -0300

    MantenimientoControllerApi

---
+++ WebApi/Controllers/MantenimientosController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MantenimientosController : ControllerBase
+    {
+        // GET: api/<MantenimientosController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        // GET api/<MantenimientosController>/5
+        [HttpGet("{id}", Name = "Get")]
+        public IActionResult Get(int id)
+        {
+            return Ok();
+        }
+
+        // POST api/<MantenimientosController>
+        [HttpPost]
+        public IActionResult Post([FromBody] string value)
+        {
+            return CreatedAtRoute("Get", new { id=0}, null);
+        }
+
+        // PUT api/<MantenimientosController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] string value)
+        {
+            return Ok();
+        }
+
+        // DELETE api/<MantenimientosController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return NoContent();
+        }
+    }
+}
